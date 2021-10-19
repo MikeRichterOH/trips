3da85ab2cc4b8c390d2fcd8e2a41382eb161b35c
richterdev <92743509+richterdev@users.noreply.github.com>
2021-10-18 20:46:03 -0400

    Update Program.cs
    Fix #12

--- apis/poi/web/Program.cs
+++ apis/poi/web/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting; **THIS IS A BAD CHANGE!!!**
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
